0cbbfd21fc1ee83a87a72abed0b73b5bfdf56bc4
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-04-24 12:40:25 +0200

    changes on front

--- src/components/UrlFormHome.jsx
+++ src/components/UrlFormHome.jsx
@@ -4,8 +4,7 @@ import { SERVER_ENDPOINTS } from "../services/endpoints";
 import CopyToClipboard from "react-copy-to-clipboard";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import { Alert } from "@mui/material";
-import imageHome from "../assets/img/getSpace_Home.png";
-import LogoImg from "../assets/img/logoPlanet.png";
+import LogoImg from "../assets/img/rocket.png";
 
 function UrlFormHome() {
   const [url, setUrl] = useState();
@@ -17,8 +16,9 @@ function UrlFormHome() {
     const timer = setTimeout(() => {
       setCopied(false);
     }, 2000);
-
-    return () => clearTimeout(timer);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [copied]);
 
   async function handleSubmit(e) {
@@ -36,9 +36,12 @@ function UrlFormHome() {
   return (
     <div className="background">
       <div className="navbar">
-        <div className="navleft">
-          <img src={LogoImg} alt="logo" className="logo" />
-          <p>getSpace</p>
+        <div className="nav">
+          <div className="navleft">
+            <img src={LogoImg} alt="logo" className="logo" />
+            <p>getSpace</p>
+          </div>
+          <p>Account</p>
         </div>
         <Alert
           className={copied === false ? "notVisible " : ""}
@@ -47,54 +50,67 @@ function UrlFormHome() {
           Url successfully copied!
         </Alert>
       </div>
-
+      <div className="signup">
+        <input type="button" value="Sign up" className="signup" />
+      </div>
       <div className="page">
         <div className="form">
           <form onSubmit={handleSubmit}>
-            <h1>Get Space URL Generator</h1>
+            <h1 className="title">
+              Get Space <br />
+              <span>URL Generator</span>
+            </h1>
             <p>
               Get Space URL shortener build to generate short links that creates
               better click impression{" "}
             </p>
-            <input
-              type="text"
-              name="longUrl"
-              className="longUrl"
-              id="longUrl"
-              placeholder="https://example.com"
-              onChange={(e) => setUrl(e.target.value)}
-            />
-            <br />
-            <br />
-            <button type="submit" className="button">
-              Create a link
-            </button>
+            <div className="center">
+              <input
+                type="text"
+                name="longUrl"
+                className="longUrl"
+                id="longUrl"
+                placeholder="https://example.com"
+                onChange={(e) => setUrl(e.target.value)}
+              />
+              <br />
+              <br />
+
+              <button type="submit" className="button">
+                <span>Create</span>
+                <div class="line"></div>
+                <div class="line-2"></div>
+                <div class="speak"></div>
+                <div class="speak one"></div>
+                <div class="speak two"></div>
+                <div class="speak three"></div>
+              </button>
+            </div>
           </form>
           <br />
           {obj.length === 0 ? (
             <div className="notVisible"></div>
           ) : (
-            <div className="shortLink">
-              <p>
-                <a
-                  className="box"
+            <div>
+              <CopyToClipboard text={shortUrl} onCopy={() => setCopied(true)}>
+                <div
+                  className="card"
                   href={obj.fullUrl}
                   target={"_blank noreferrer"}
                 >
-                  {shortUrl}
-                </a>
-              </p>
-              <CopyToClipboard text={shortUrl} onCopy={() => setCopied(true)}>
-                <ContentCopyIcon
-                  fontSize="small"
-                  className={copied ? "copied" : ""}
-                  onClick={console.log("copiado")}
-                />
+                  <p className={copied ? "copied" : ""}>{shortUrl}</p>
+
+                  <ContentCopyIcon
+                    fontSize="small"
+                    className={copied ? "copied" : ""}
+                    onClick={console.log("copiado")}
+                  />
+                </div>
               </CopyToClipboard>
             </div>
           )}
         </div>
-        <img src={imageHome} alt="Home" className="imageHome" />
+        {/* <img src={imageHome} alt="Home" className="imageHome" /> */}
       </div>
     </div>
   );
