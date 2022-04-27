13ba089d3bb3f8a117a106f0c71dea0fc166b6b1
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-04-27 16:42:15 +0200

    render shorturl in a new page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SignUp from "./components/SignUp";
 import UrlFormHome from "./components/UrlFormHome";
 import SignIn from "./components/SignIn";
 import Analytics from "./components/Analytics";
+import ShortUrl from "./components/ShortUrl";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<UrlFormHome />} />
+          <Route path="/:shortUrl" element={<ShortUrl />} />
           <Route path="/signUp" element={<SignUp />} />
           <Route path="/signIn" element={<SignIn />} />
           <Route path="/analytics" element={<Analytics />} />
