05d9fd95e0392298252aa18176872e2aa9fadbeb
patriglezperez <patriciaggonzalezperez@hotmail.com>
2022-05-14 21:35:22 +0200

    error component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UrlFormHome from "./components/UrlFormHome/UrlFormHome";
 import ShortUrl from "./components/ShortUrl/ShortUrl";
+import Error404 from "./components/Error404/Error404";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<UrlFormHome />} />
           <Route path="/:shortUrl" element={<ShortUrl />} />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/components/UrlFormHome/UrlFormHome.jsx
+++ src/components/UrlFormHome/UrlFormHome.jsx
@@ -7,12 +7,14 @@ import { Alert } from "@mui/material";
 import phone from "../../assets/img/phone.webp";
 import iphone from "../../assets/img/iphoneGSpsd.png";
 import logo from "../../assets/img/gsLogo.png";
+import { useNavigate } from "react-router-dom";
 
 function UrlFormHome() {
   const [url, setUrl] = useState();
   let [shortUrl, setShortUrl] = useState([]);
   let [obj, setObj] = useState([]);
   const [copied, setCopied] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -37,7 +39,21 @@ function UrlFormHome() {
     <div>
       <div className="navbar">
         <div className="navleft">
-          <img src={logo} alt="logo" className="logo" />
+          <svg
+            height="24"
+            width="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path
+              d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+              fill="currentColor"
+            ></path>
+          </svg>
           <p className="left-logo">getSpace</p>
         </div>
         <Alert
@@ -60,7 +76,7 @@ function UrlFormHome() {
                   lugar donde todo sucede
                 </h1>
                 <p>
-                  Get Space es el acortador de urls con el que conseguirás
+                  GetSpace es el acortador de urls con el que conseguirás
                   generar urls cortas que crean una mejor impresión. Es fácil.
                   Agregra tu url y haz clic en "Crear".
                 </p>
